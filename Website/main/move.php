942e6e8f2cdd0e44fdaf3eae89ba5fe074e8ea43
Conner H <connerhi@mtu.edu>
2026-03-09 15:44:08 -0400

    added login query

--- Website/main/move.php
+++ Website/main/move.php
@@ -1,9 +1,32 @@
 <?php
 
 
+
+    try 
+    {
+        $sql = "SELECT username, password FROM Users WHERE username = {$_POST['username']} AND password = {$_POST['password']}}";
+        // Execute the SQL query
+        $result = $conn->query($sql);
+        // Process the result set
+        if ($result->rowCount() > 0) 
+        {
+            echo "Account Found!";
+        }
+        else
+        {
+            echo "No account found...";
+        }
+    } 
+    catch(PDOException $e) 
+    {
+    echo "Error: " . $e->getMessage();
+    }
+
+
+/*
     if ($_POST["username"] != NULL)
         header('Location: '. 'index.html');
-        die();
+        //die();*/
 
 
 ?>
