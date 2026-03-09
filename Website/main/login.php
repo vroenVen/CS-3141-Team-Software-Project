35f3082e49dd8ab55d71c698730f02a0a29e2499
Conner Hill <connerhi@mtu.edu>
2026-03-09 13:04:20 -0400

    redirect after login

---
+++ Website/main/login.php
@@ -0,0 +1,37 @@
+<?php
+
+    //$_POST["username"] = "Default Username";
+    //$_POST["password"] = "Defulat Password";
+
+    include "move.php";
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html class="align">
+    <head>
+        <link href="primary.css" media="all" rel="Stylesheet" type="text/css" />
+        <script src="db.js"></script>
+    </head>
+
+    <body class="align">
+        <div class="container-login">
+            <div class="login">
+                <div>
+                    <form action="move.php" method="post">
+                        <label>Username:</label>
+                        <input class="input-login" type="text" placeholder="Enter Username" name="username" required>
+                        <label>Password:</label>
+                        <input class="input-login" type="password" placeholder="Enter Password" name="password" required>
+                        <input type="submit" value="Login">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+
+
+</html>
---
+++ Website/main/move.php
@@ -0,0 +1,8 @@
+<?php
+
+
+    if ($_POST["username"] != NULL)
+        header('Location: '. 'index.html');
+
+
+?>
