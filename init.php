17d4992cebec2bba27ac220cf72a4c36fecd852a
Conner H <connerhi@mtu.edu>
2026-03-11 00:07:15 -0400

    Created db connection

---
+++ init.php
@@ -0,0 +1,36 @@
+<?php
+
+    class Dbh
+    {
+
+        private $servername;
+        private $username;
+        private $password;
+        private $dbname;
+        private $charset;
+
+        public function connect()
+        {
+            $this->servername = "localhost";
+            $this->username = "apache";
+            $this->password = "knockknockLetM3!n";
+            $this->dbname = "tsp";
+            $this->charset = "utf8mb4";
+
+            try
+            {
+                $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
+                $pdo = new PDO($dsn, $this->username, $this->password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //echo "Successfully connected!";
+                return $pdo;
+            }
+            catch (Exception $e)
+            {
+                echo "Connection failed: ".$e->getMessage();
+            }
+        }
+
+    }
+
+?>
