24c11ea064b8ebb393b8913602d070b5e31dc914
Conner H <connerhi@mtu.edu>
2026-03-11 00:09:12 -0400

    Created index.php to replace index.html

---
+++ Website/main/index.php
@@ -0,0 +1,56 @@
+<?php
+
+     session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<link href="primary.css" media="all" rel="Stylesheet" type="text/css" />
+
+</head>
+<body>
+
+<ul>
+  <li onclick="handleClick(this)" ><a href="#new">+ New</a></li>
+  <li onclick="handleClick(this)" ><a href="#home">Files</a></li>
+  <?php
+  if ($_SESSION["username"] != NULL) { ?>
+     <li onclick="handleClick(this)" style="float:right"><a href="#profile">Profile</a></li>
+     <li onclick="handleClick(this)" style="float:right"><a href="logout.php">Logout</a></li>
+  <?php } else { ?>
+     <li onclick="window.location.href='login.php'" style="float:right"><a href="#profile">Login</a></li>
+  <?php } ?>
+</ul>
+
+<div id="new">
+     <ul class="save-bar">
+          <li onclick="handleClick(this)" ><a href="#">Save</a></li>
+     </ul>
+  <textarea id="more-info" class="hidden-content"></textarea>
+</div>
+
+</body>
+</html>
+
+<script>
+
+let activeClick = null;
+
+function handleClick(element) {
+   element.classList.toggle("active");
+
+   if(activeClick && activeClick != element){
+        activeClick.classList.toggle("active");
+   }
+   if(activeClick == element){
+        activeClick = null;
+   } else{
+        activeClick = element;
+   }
+
+}
+
+</script>
