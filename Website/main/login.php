4c162eac86480f979e703cd871fd0d322589a5c2
AsrielCreemurr <asrielcreemurr@gmail.com>
2026-03-16 18:36:12 -0400

    users can save files. uid stored in PHP session

--- Website/main/login.php
+++ Website/main/login.php
@@ -33,6 +33,9 @@
                 if (password_verify($password, $db_password))
                 {
                     $_SESSION["username"] = $username;
+					$stmt = $conn->prepare("SELECT id FROM Users WHERE username=:username");
+					$stmt->bindParam(":username", $username);
+					$_SESSION["uid"] = $stmt->execute();
                     header('location: index.php');
                     die();
                 }
