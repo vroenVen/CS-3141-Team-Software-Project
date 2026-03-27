24d7bd3697b2ee9d23e8d6a689e67e4c060111d6
Conner Snoddy <csnoddy@mtu.edu>
2026-03-27 13:58:15 -0400

    move db creds out of repo

--- init.php
+++ init.php
@@ -11,16 +11,11 @@
 
         public function connect()
         {
-            $this->servername = "localhost";
-            $this->username = "apache";
-            $this->password = "knockknockLetM3!n";
-            $this->dbname = "tsp";
-            $this->charset = "utf8mb4";
+			$config = parse_ini_file("../db.ini");
 
             try
             {
-                $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
-                $pdo = new PDO($dsn, $this->username, $this->password);
+                $pdo = new PDO($config['dsn'], $config['username'], $config['password']);
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 //echo "Successfully connected!";
                 return $pdo;
