6ab06a28bc4e277411f2b7172c4b0c55eff577e5
Conner Snoddy <csnoddy@mtu.edu>
2026-03-27 14:17:38 -0400

    Revert "move db creds out of repo"
    This reverts commit 24d7bd3697b2ee9d23e8d6a689e67e4c060111d6.
    
    needs testing

--- init.php
+++ init.php
@@ -11,11 +11,16 @@
 
         public function connect()
         {
-			$config = parse_ini_file("../db.ini");
+            $this->servername = "localhost";
+            $this->username = "apache";
+            $this->password = "knockknockLetM3!n";
+            $this->dbname = "tsp";
+            $this->charset = "utf8mb4";
 
             try
             {
-                $pdo = new PDO($config['dsn'], $config['username'], $config['password']);
+                $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname.";charset=".$this->charset;
+                $pdo = new PDO($dsn, $this->username, $this->password);
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 //echo "Successfully connected!";
                 return $pdo;
